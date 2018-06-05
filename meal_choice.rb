a69fef126ab53df3213b12afbb89c772d98b571f
Mehreen Butt <mbutt@republiccharterschools.org>
2018-06-05 04:03:19 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -29,6 +29,6 @@ puts dinner("broccoli")
 puts snacks
 ## In our snacks method, the default choice is "Cheetos" Calling snacks like this without specifying a choice will return "Any time, is the right time for Cheetos!" 
 
-breakfast
-lunch
-dinner
+puts breakfast
+puts lunch
+puts dinner
